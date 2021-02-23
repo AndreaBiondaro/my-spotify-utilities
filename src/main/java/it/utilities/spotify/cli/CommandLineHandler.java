0be5d7ee5ed537a97e0277aa11674638d4b09dfe
AndreaBiondaro <andrea.biondaro89@gmail.com>
2021-02-23 21:23:34 +0100

    Pom updated to add test dependencies

--- src/main/java/it/utilities/spotify/cli/CommandLineHandler.java
+++ src/main/java/it/utilities/spotify/cli/CommandLineHandler.java
@@ -1,20 +1,27 @@
 package it.utilities.spotify.cli;
 
 import java.util.Scanner;
+import java.util.function.Consumer;
 
 public class CommandLineHandler {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        String command = null;
+  private static boolean running = true;
 
-        do {
-            System.out.print("Command: ");
-            command = scanner.nextLine();
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
-            System.out.println(command);
-        } while (!command.equalsIgnoreCase("exit"));
+    do {
+      System.out.print("Command: ");
+      handleInputCommand(scanner.nextLine());
+    } while (running);
+  }
 
-        System.out.println("Close");
+  private static void handleInputCommand(String command) {
+    Consumer consumer = null;
+
+    if (command.equalsIgnoreCase("exit")) {
+      running = false;
+      System.out.println("Closing");
     }
+  }
 }
