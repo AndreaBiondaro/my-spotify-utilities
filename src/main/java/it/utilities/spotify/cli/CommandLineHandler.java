9a7c76c9a9ca9c92c328f12452494c0983b3ac63
AndreaBiondaro <andrea.biondaro89@gmail.com>
2021-03-02 18:34:44 +0100

    Added some comments and added comand to print help

--- src/main/java/it/utilities/spotify/cli/CommandLineHandler.java
+++ src/main/java/it/utilities/spotify/cli/CommandLineHandler.java
@@ -19,6 +19,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
+/** Instances of the CommandLineHandler class provide access to methods for handling user input. */
 public class CommandLineHandler {
 
   private boolean running = true;
@@ -41,6 +42,10 @@ public class CommandLineHandler {
     handler.startReadingInput();
   }
 
+  /**
+   * This method is the starting point from which the program listens for user input. Execution will
+   * continue as long as the <code>running</code> attribute is <code>true</code>.
+   */
   public void startReadingInput() {
     do {
       System.out.print("Command: ");
@@ -48,8 +53,14 @@ public class CommandLineHandler {
     } while (running);
   }
 
+  /**
+   * Manage user input.
+   *
+   * @param command indicates what the user writes to the console.
+   */
   private void handleInputCommand(String command) {
-    Consumer consumer = null;
+    // Operation that must be performed
+    Consumer<String[]> consumer = null;
 
     if (command == null || command.isBlank()) {
       return;
@@ -58,24 +69,10 @@ public class CommandLineHandler {
     String[] args = command.split("\\s+");
     command = args[0];
 
-    // TODO: need to understand how see the playlists that are private
-
-    // authorization-request --> client id, client secret, redirect url --> open browser if possible
-    // or print the url generated
-
-    // generate-tokens* --> code --> request return access token and refresh token. Print refresh
-    // token than user can save somewhere
-
-    // refresh-token* --> client id, client-secret, refresh token --> this is used if user already
-    // have the access token
-
-    //    * --> before access token expire need to make a request to give another one
-
-    // ScheduledExecutorService
-
     switch (command.toLowerCase()) {
       case "manual":
       case "help":
+        consumer = printHelp();
         break;
       case "generate-authorization-url":
         consumer = generateAuthorizationUrl();
@@ -115,16 +112,81 @@ public class CommandLineHandler {
     }
   }
 
-  private void scheduleRefreshTokenOperation(Integer time) {
-    this.scheduledExecutorService.schedule(
-        () -> {
-          // TODO refresh access token
-        },
-        time,
-        TimeUnit.MILLISECONDS);
-    // TODO change the time unit to seconds
+  /**
+   * Generate a help message with the description of all the commands managed by the program.
+   *
+   * @return
+   */
+  private Consumer<String[]> printHelp() {
+    return args -> {
+      final String commandFormat = "\t\t%-50s %s\n";
+      final String helpMessage =
+          "Manual for using Spotify utilities.\n"
+              + "\tTo use one of the commands, you need to type the command you want and the arguments (if needed).\n"
+              + "\tTo see if the command requires arguments, use the command and if arguments are needed, print an error message.\n"
+              + "\tList of current commands:\n"
+              + String.format(commandFormat, "help, manual", "Print all commands.")
+              + String.format(
+                  commandFormat,
+                  "generate-authorization-url",
+                  "Generate URL, which you need to use to authorize this application to access Spotify data.")
+              + String.format(
+                  commandFormat,
+                  "generate-tokens",
+                  "Given the authorization code, it retrieves the access token and the refresh token.")
+              + String.format(
+                  commandFormat,
+                  "use-refresh-token",
+                  "If you already have the refresh token, you can use it for configuration. However, you must first use the command \"generate-authorization-url\"")
+              + String.format(
+                  commandFormat, "get-user-playlists", "Returns all playlists of the current user.")
+              + String.format(
+                  commandFormat,
+                  "playlist-duplicate-elements",
+                  "Returns all tracks that are duplicated in a playlist.")
+              + String.format(commandFormat, "exit, quit", "Finish the execution.");
+
+      System.out.println(helpMessage);
+    };
   }
 
+  /**
+   * Perform the Access Token Refresh operation before it expires.
+   *
+   * @param time the time from now to delay execution
+   */
+  private void scheduleRefreshTokenOperation(Integer time) {
+    this.scheduledExecutorService.schedule(refreshAccessToken(), time, TimeUnit.SECONDS);
+  }
+
+  /**
+   * Make the call to retrieve the new access token.
+   *
+   * @return
+   */
+  private Runnable refreshAccessToken() {
+    return () -> {
+      AuthorizationCodeCredentials authorizationCodeCredentials = null;
+      try {
+        authorizationCodeCredentials = this.spotifyApiWrapper.authorizationCode();
+      } catch (Exception e) {
+        System.err.println(e.getMessage());
+        System.out.println("Error while trying to retrieve access token. Please try again.");
+      }
+
+      if (authorizationCodeCredentials != null) {
+        this.builder.setAccessToken(authorizationCodeCredentials.getAccessToken());
+        scheduleRefreshTokenOperation(authorizationCodeCredentials.getExpiresIn());
+      }
+    };
+  }
+
+  /**
+   * Generate the URL that the user must use to authorize the program to use Spotify API.<br>
+   * If the platform supports the browser, it automatically opens to the generated URL.
+   *
+   * @return
+   */
   private Consumer<String[]> generateAuthorizationUrl() {
     return args -> {
       if (args == null || args.length < 3) {
@@ -139,9 +201,7 @@ public class CommandLineHandler {
 
         // The user can indicate whether he wants to open the browser to authenticate himself or
         // print the url only on the console.
-        boolean openBrowser =
-            (args.length > 3 && Boolean.TRUE.toString().equalsIgnoreCase(args[3]))
-                || args.length <= 3;
+        boolean openBrowser = args.length <= 3 || Boolean.TRUE.toString().equalsIgnoreCase(args[3]);
 
         if (Desktop.isDesktopSupported()
             && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)
@@ -158,6 +218,11 @@ public class CommandLineHandler {
     };
   }
 
+  /**
+   * Performs the request to retrieve the access token.
+   *
+   * @return
+   */
   private Consumer<String[]> generateTokens() {
     return args -> {
       if (args == null || args.length < 1) {
@@ -184,6 +249,11 @@ public class CommandLineHandler {
     };
   }
 
+  /**
+   * Set the refresh token to be used.
+   *
+   * @return
+   */
   private Consumer<String[]> useRefreshToken() {
     return args -> {
       if (args == null || args.length < 1) {
@@ -191,23 +261,16 @@ public class CommandLineHandler {
             "Syntax error. To use this command you need to pass other arguments.\nSyntax: use-refresh-token <refresh-code>");
       } else {
         this.builder.setRefreshToken(args[0]);
-
-        AuthorizationCodeCredentials authorizationCodeCredentials = null;
-        try {
-          authorizationCodeCredentials = this.spotifyApiWrapper.authorizationCode();
-        } catch (Exception e) {
-          System.err.println(e.getMessage());
-          System.out.println("Error while trying to retrieve access token. Please try again.");
-        }
-
-        if (authorizationCodeCredentials != null) {
-          this.builder.setAccessToken(authorizationCodeCredentials.getAccessToken());
-          scheduleRefreshTokenOperation(authorizationCodeCredentials.getExpiresIn());
-        }
+        refreshAccessToken().run();
       }
     };
   }
 
+  /**
+   * Retrieve the playlist list of the current user.
+   *
+   * @return
+   */
   private Consumer<String[]> getUserPlaylists() {
     return args -> {
       Paging<PlaylistSimplified> playlists = null;
@@ -221,11 +284,18 @@ public class CommandLineHandler {
       }
 
       if (playlists != null && playlists.getItems() != null && playlists.getItems().length > 0) {
-        Arrays.stream(playlists.getItems()).forEach(playlist -> System.out.println(playlist));
+        Arrays.stream(playlists.getItems()).forEach(System.out::println);
+      } else {
+        System.out.println("Nothing to show");
       }
     };
   }
 
+  /**
+   * Recover duplicate tracks for a specific playlist.
+   *
+   * @return
+   */
   private Consumer<String[]> getPlaylistDuplicateElements() {
     return args -> {
       if (args == null || args.length < 1) {
@@ -242,7 +312,7 @@ public class CommandLineHandler {
         }
 
         if (tracks != null && !tracks.isEmpty()) {
-          tracks.stream().forEach(track -> System.out.println(track));
+          tracks.forEach(System.out::println);
         } else {
           System.out.println("Nothing to show");
         }
@@ -250,13 +320,19 @@ public class CommandLineHandler {
     };
   }
 
-  private Consumer terminateExecution() {
-    return s -> {
+  /**
+   * Stop program execution.
+   *
+   * @return
+   */
+  private Consumer<String[]> terminateExecution() {
+    return args -> {
       System.out.print(
           "Are you sure you want to end the session? If you want to open another one you have to re-authenticate. (Y/N) ");
       String answer = scanner.nextLine();
 
       if (answer.equalsIgnoreCase("Y")) {
+        this.scheduledExecutorService.shutdownNow();
         running = false;
         System.out.println("Terminated");
       }
